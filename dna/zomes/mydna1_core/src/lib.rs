3869f0e84646334c77f08efd9a1e6688f8bf4d26
Eric Harris-Braun <eric@harris-braun.com>
2022-03-21 17:20:47 -0400

    Wip

--- dna/zomes/mydna1_core/src/lib.rs
+++ dna/zomes/mydna1_core/src/lib.rs
@@ -1,14 +1,11 @@
-pub use hdk::prelude::*;
+use std::u8;
 
+pub use hdk::prelude::*;
 
 ////////////////////////////////////////////////////////////////////////////////
 // Entry declarations
 ////////////////////////////////////////////////////////////////////////////////
-entry_defs![
-    MyEntry1::entry_def(),
-    MyEntry2::entry_def()
-];
-
+entry_defs![MyEntry1::entry_def(), MyEntry2::entry_def()];
 
 ////////////////////////////////////////////////////////////////////////////////
 // Entry struct definitions with necessary impls
@@ -18,7 +15,6 @@ entry_defs![
 pub struct MyEntry1 {
     pub thing1: String,
 }
-
 
 #[hdk_entry(id = "my_entry2")]
 #[derive(Clone)]
@@ -35,22 +31,63 @@ impl MyEntry2 {
 // Link Types
 ////////////////////////////////////////////////////////////////////////////////
 
-
+pub enum MyLink1 {
+    Fish = 0,
+    Dog,
+    Cow
+}
+impl From<LinkType> for MyLink1 {
+    fn from(x: LinkType) -> Self {
+        match x.0 {
+            0 => MyLink1::Fish,
+            1 => MyLink1::Dog,
+            2 => MyLink1::Cow,
+        }
+    }
+}
 ////////////////////////////////////////////////////////////////////////////////
 // Validation callback
 ////////////////////////////////////////////////////////////////////////////////
 
-
 #[hdk_extern]
 pub fn validate(op: Op) -> ExternResult<ValidateCallbackResult> {
+    let info = zome_info()?;
     match op {
-        Op::StoreElement { element: _ } => Ok(ValidateCallbackResult::Valid),
-        Op::StoreEntry { .. } => Ok(ValidateCallbackResult::Valid),
-        Op::RegisterCreateLink {
-            create_link: _,
-        } => {
+        Op::StoreElement { element } => {
+            match element.header() {
+                Header::Dna(_) => todo!(),
+                Header::AgentValidationPkg(_) => todo!(),
+                Header::InitZomesComplete(_) => todo!(),
+                Header::CreateLink(create) => match create.link_type.into() {
+                    MyLink1::Fish => todo!(),
+                    _ => {}
+                },
+                Header::DeleteLink(_) => todo!(),
+                Header::OpenChain(_) => todo!(),
+                Header::CloseChain(_) => todo!(),
+                Header::Create(create) => match create.entry_type {
+                    EntryType::AgentPubKey => todo!(),
+                    EntryType::App(app_entry_type) => {
+                        match info.entry_defs.get(app_entry_type.id.index()).map(|entry_def| entry_def.id.to_string()) {
+                            "something" => _
+                        }
+                    }
+                    EntryType::CapClaim => todo!(),
+                    EntryType::CapGrant => todo!(),
+                },
+                Header::Update(_) => todo!(),
+                Header::Delete(_) => todo!(),
+            }
             Ok(ValidateCallbackResult::Valid)
         }
+        Op::StoreEntry { header, .. } => {
+            match header.hashed.content.entry_type() {
+                entry_def_index!(String::from("somethings")) => todo!(),
+                _ => {}
+            }
+            Ok(ValidateCallbackResult::Valid)
+        }
+        Op::RegisterCreateLink { create_link: _ } => Ok(ValidateCallbackResult::Valid),
         Op::RegisterDeleteLink { create_link: _, .. } => Ok(ValidateCallbackResult::Invalid(
             "deleting links isn't valid".to_string(),
         )),
