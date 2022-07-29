988a7880ba7c5146cb7fbe1b91f0428dd6a2f17c
Perry Kundert <perry@dominionrnd.com>
2022-07-29 11:03:14 -0600

    Tests pass, including both successful/invalid tests

--- tests/integration/test_api.js
+++ tests/integration/test_api.js
@@ -6,8 +6,15 @@ const log				= require('@whi/stdlog')(path.basename( __filename ), {
 
 const fs				= require('fs');
 const expect				= require('chai').expect;
+const { expect_reject }                 = require('./utils.js');
 const { HoloHash }			= require('@whi/holo-hash');
 const { Holochain }			= require('@whi/holochain-backdrop');
+const { ConductorError,
+	EntryNotFoundError,
+	DeserializationError,
+	CustomError,
+	...hc_client }			= require('@whi/holochain-client');
+
 const json				= require('@whi/json');
 
 const { backdrop }			= require('./setup.js');
@@ -19,73 +26,23 @@ let clients;
 
 
 function basic_tests () {
-    it("should create an entry", async function () {
+    it("should create an entry with valid text", async function () {
 	this.timeout( 10_000 );
 
         let addr			= new HoloHash( await clients.alice.call( "integrity-template-dna", "integrity-template-coordinator-zome", "create", "Hello, world!" ) );
 	log.normal("New entry address: %s", String(addr) );
 
     });
-  /*
-    const input			= Buffer.from("Somewhere over the rainbow");
-    let memory_addr;
-    let memory;
-
-    it("should create a memory using 'save_bytes'", async function () {
-	this.timeout( 10_000 );
-
-	let addr			= new HoloHash( await clients.alice.call( "memory", "mere_memory", "save_bytes", input ) );
-	log.normal("New memory address: %s", String(addr) );
-
-	memory_addr			= addr;
-    });
-
-    it("should get a memory using 'retrieve_bytes'", async function () {
-	this.timeout( 10_000 );
-
-	memory				= await clients.alice.call( "memory", "mere_memory", "get_memory", memory_addr );
-	log.normal("New memory: %s", json.debug(memory) );
-    });
-
-    it("should calculate hash of the memory bytes", async function () {
-	this.timeout( 10_000 );
-
-	{
-	    let hash			= await clients.alice.call( "memory", "mere_memory", "calculate_hash", input );
-	    log.normal("Calculated hash: %s", hash );
-
-	    expect( hash		).to.deep.equal( memory.hash );
-	}
-
-	{
-	    let hash			= await clients.alice.call( "memory", "mere_memory", "calculate_hash", Buffer.from("hello world") );
-	    log.normal("Calculated hash: %s", hash );
-
-	    expect( hash		).to.equal("b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9");
-	}
-    });
-
-    it("should find a memory based on the hash", async function () {
-	this.timeout( 10_000 );
-
-	{
-	    let exists			= await clients.alice.call( "memory", "mere_memory", "memory_exists", input );
-	    log.normal("Memory exists: %s", exists );
-
-	    expect( exists		).to.be.true;
-	}
-
-	{
-	    let exists			= await clients.alice.call( "memory", "mere_memory", "memory_exists", Buffer.from("hello world") );
-	    log.normal("Memory exists: %s", exists );
-
-	    expect( exists		).to.be.false;
-	}
-    });
-  */
 }
 
 function errors_tests () {
+    it("should fail creating an entry with known-invalid text", async function () {
+        this.timeout( 10_000 )
+
+        await expect_reject( async () => {
+            await clients.alice.call( "integrity-template-dna", "integrity-template-coordinator-zome", "create", "invalid text 1" );
+        }, ConductorError, "Source chain error: InvalidCommit error: invalid thing1" );
+    });
 }
 
 describe("Zome: Integrity Template", () => {
